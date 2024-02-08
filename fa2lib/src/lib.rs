dd5639a6cfef25236c8638f3cee3102a64dc3a37
Santo Shakil <shakil.mezbah@gmail.com>
2024-02-08 18:33:04 +0600

    WIP:

--- fa2lib/src/lib.rs
+++ fa2lib/src/lib.rs
@@ -2,5 +2,8 @@ use std::os::raw::c_uint;
 
 #[no_mangle]
 pub extern "C" fn add(left: c_uint, right: c_uint) -> c_uint {
+    let current_dir = std::env::current_dir().unwrap();
+    let current_dir = std::fs::canonicalize(current_dir).unwrap();
+    println!("Current dir:: {:?}", current_dir);
     (left + right + 100) as c_uint
 }
